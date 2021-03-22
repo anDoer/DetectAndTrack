9927e70d4d7ad99d1f694e72c02486cbc8a05fdd
anDoer <16701672+anDoer@users.noreply.github.com>
2021-03-22 15:34:53 +0100

    as

--- lib/cmake/legacy/legacymake.cmake
+++ lib/cmake/legacy/legacymake.cmake
@@ -1,3 +1,18 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##############################################################################
+
 # This file contains legacy cmake scripts that is going to be removed
 # in a future release.
 
@@ -6,7 +21,7 @@ list(APPEND CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/cmake/legacy/Modules)
 
 # Add compiler flags.
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c11")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -O2 -fPIC -Wno-narrowing")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14 -O2 -fPIC -Wno-narrowing")
 
 # Include Caffe2 CMake utils.
 include(cmake/legacy/Utils.cmake)
